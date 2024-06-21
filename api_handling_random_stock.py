3cadc893ea24fc873e802244bf95cc07aee47e0b
Abhi0496 <aaagrawal.abhishek@gmail.com>
2024-06-20 21:02:46 -0700

    Adding code for getting random stock

---
+++ api_handling_random_stock.py
@@ -0,0 +1,29 @@
+import requests
+
+def random_stock():
+    url = "https://api.freeapi.app/api/v1/public/stocks/stock/random"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    if data["success"] and "data" in data:
+        name = data["data"]["Name"]
+        symbol = data["data"]["Symbol"]
+        current_price = data["data"]["CurrentPrice"]
+        return name,symbol,current_price
+    else:
+        raise Exception("Data not available")
+
+def main():
+    try:
+        name,symbol,current_price = random_stock()
+        print(f"Name: {name} \nSymbol: {symbol} \nCurrent Price: {current_price}")
+    except Exception as e:
+        print(str(e))
+
+    pass
+    pass
+
+if __name__ == "__main__":
+    main()
